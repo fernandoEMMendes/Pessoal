d62643e405d03a0cdb4547e151e8f5efe8ac72d9
Fernando <fernandoeduardomottamendes@gmail.com>
2023-08-18 17:23:18 -0300

    asasd

--- MeusTestes/src/Pages/AloneDark/index.js
+++ MeusTestes/src/Pages/AloneDark/index.js
@@ -7,26 +7,31 @@ import React, { useState } from "react"
 function AloneDark() {
 
   //Cont = Contador
-  const [Frases, setFrases] = useState("I can't see anything.")
   const [OnHand, setOnHand] = useState("None")
-  const [ContGraveto, setContGraveto] = useState(0)
 
+  const [ContGraveto, setContGraveto] = useState(0)
+  const [ContPedra, setContPedra] = useState(0)
 
   const [Torch, setTorch] = useState(false)
 
 
-  
-  
-  function Coletar() {
-    if (ContGraveto >= 5) { setContGraveto(ContGraveto + 0) }
-    else setContGraveto(ContGraveto + 1)
 
-    if (ContGraveto >= 5) { setFrases("It's too heavy, I can't carry anymore") }
+
+  function Coletar() {
+    if (!Torch) { setContGraveto(ContGraveto + 1) }
+
+    if (Torch) {
+      if (Math.round(Math.random() * 10) < 5) {
+        setContGraveto(ContGraveto + 1)
+      } else {
+        setContPedra(ContPedra + 1)
+      }
+    }
   }
 
-  
-  
-  
+
+
+
   function CraftTorch() {
     if (Torch) {
       alert("I don't need another one.")
@@ -35,26 +40,49 @@ function AloneDark() {
 
     if (ContGraveto < 5) { alert("Not enough resources") }
     else { setContGraveto(ContGraveto - 5) || setOnHand("Torch") || setTorch((current) => !current) }
-    
+
   }
 
-  
-  
-  
-  
+
+
+
+
   return (
-
     <div>
-      <h1>{Torch}</h1>
-      <h3>{Frases}</h3> <br />
-      <p>Equip: {OnHand}</p> <br /> <br />
+      <div class="Margin">
 
-      <h2>{ContGraveto}</h2>
-      <button onClick={Coletar}>Gather sticks</button> <br /> <br />
+        <div>
 
-      {!Torch && <button onClick={CraftTorch}>Torch (5 Sticks)</button>}
-      {Torch && <a class="TextAcquired">Torch acquired</a>}
+          {/*Caixa de equipamentos*/}
+          {Torch &&
+            <div>
+              <a>Equipped</a>
 
+              <select>
+                <option onChange={(e) => setOnHand(e.target.value)} value="none">None</option>
+                <option onChange={(e) => setOnHand(e.target.value)} value="Torch">Torch</option>
+              </select>
+
+            </div>
+          } <br /> <br />
+
+          <h2>Sticks: {ContGraveto}</h2>
+          {!Torch && <button onClick={Coletar}>Gather sticks</button>}
+
+          {Torch && <h2>Rocks: {ContPedra}</h2>}
+          {Torch && <button onClick={Coletar}>Gather materials</button>}
+
+        </div>
+
+        <br /> <br />
+
+        <div>
+
+          {!Torch && <button onClick={CraftTorch}>Torch (5 Sticks)</button>}
+
+        </div >
+
+      </div>
     </div>
   );
 }
