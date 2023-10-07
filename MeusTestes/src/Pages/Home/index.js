71facc76d531e05375daa2d2ba2257e9d5e21d7f
Cappucino <fernandoeduardomottamendes@gmail.com>
2023-10-07 13:19:19 -0300

    435345345

--- MeusTestes/src/Pages/Home/index.js
+++ MeusTestes/src/Pages/Home/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div>
             <Link to="/RPGTeste"><h2>RPGTeste</h2></Link>
-            <Link to="/AloneDark"><h2>AD</h2></Link>
+            <Link to="/AloneDark"><h2>AloneDark</h2></Link>
         </div>
     )
 
