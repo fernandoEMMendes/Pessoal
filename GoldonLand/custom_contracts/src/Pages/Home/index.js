09c105783376d1d66014ab52be04fd7610f16e7b
Cappucino <fernandoeduardomottamendes@gmail.com>
2023-10-31 14:58:51 -0300

    31/10/2023

--- GoldonLand/custom_contracts/src/Pages/Home/index.js
+++
@@ -1,3 +0,0 @@
-import "./Home.scss"
-
-export default function Home() {}
---
+++ GoldonLand/frontend/src/App.js
@@ -0,0 +1,9 @@
+import Rotas from "./routes"
+
+export default function App() {
+  return (
+    <div>
+      <Rotas />
+    </div>
+  )
+}
