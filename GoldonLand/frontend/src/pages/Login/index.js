6361b1e389ee63fa459c229fc01f30852f5fa647
FernandoEMMendes <fernandoeduardomottamendes@gmail.com>
2023-11-23 20:32:04 -0300

    23/11/2023

--- GoldonLand/frontend/src/pages/Login/index.js
+++ GoldonLand/frontend/src/pages/Login/index.js
@@ -1,10 +1,41 @@
+import { useState } from "react"
+
 import "./Login.scss"
+import placeholder from "../../imgs/placeholder.png"
 
 export default function Login() {
-    
+    const [nome, setNome] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    //sistema de login
+
+
     return (
-        <div className="alignTitle">
-            <h1>Login</h1>
+        <div>
+            <div className="alinharTitulo">
+                <img src={placeholder} />
+            </div>
+
+            <div className="distancia"></div>
+
+            <div className="alinharForm">
+                <form>
+                    <label>Usuário</label>
+                    <br />
+                    <input type="text" />
+
+                    <br />
+
+                    <label>Senha</label>
+                    <br />
+                    <input type="password" />
+
+                    <br /> <br />
+
+                    <button>LOGIN</button>
+                </form>
+            </div>
         </div>
     )
 }
