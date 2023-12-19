9120a613a01e4733a351e6967714ed2929f0acb0
FernandoEMMendes <fernandoeduardomottamendes@gmail.com>
2023-12-19 17:37:12 -0300

    19/12/2023

--- Projetos Senac/Mobile/aplicativo1/App.js
+++ Projetos Senac/Mobile/aplicativo1/App.js
@@ -1,4 +1,4 @@
-import { Image, ImageBackground, Button, StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Image, ImageBackground, Button, StyleSheet, Text, View, StatusBar, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -8,6 +8,7 @@ export default function App() {
 
   function Home({ navigation }) {
     return (
+
       <ImageBackground
         source={require("./src/imgs/coffeeShop.jpg")}
         style={styles.backgroundImage}>
@@ -16,76 +17,114 @@ export default function App() {
           <Text style={styles.titulo}>Café Barbosa</Text>
           <Text style={styles.subTitulo}>Desde 1998</Text>
 
+          <View style={styles.separadorGrande} />
 
           <View style={styles.separador}></View>
-          <Button title='Cardápio' onPress={() => navigation.navigate("Cardapio")} />
+          <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Cardapio")}>
+            <Text style={styles.botaoTexto}>Cardápio</Text>
+          </TouchableOpacity>
 
           <View style={styles.separador}></View>
-          <Button title='Quem Somos' onPress={() => navigation.navigate("QuemSomos")} />
+          <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("QuemSomos")}>
+            <Text style={styles.botaoTexto}>Quem Somos</Text>
+          </TouchableOpacity>
 
           <View style={styles.separador}></View>
-          <Button title='Contatos' onPress={() => navigation.navigate("Contato")} />
+          <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Contato")}>
+            <Text style={styles.botaoTexto}>Contato</Text>
+          </TouchableOpacity>
 
         </View>
       </ImageBackground>
+
     )
   }
 
   function Cardapio({ navigation }) {
     return (
-      <ImageBackground
-        source={require("./src/imgs/coffeeShop.jpg")}
-        style={styles.backgroundImage}>
+      <SafeAreaView>
+        <ScrollView>
+          <ImageBackground
+            source={require("./src/imgs/coffeeShop.jpg")}
+            style={styles.backgroundImage}>
 
-        <View style={styles.ajustarTela}>
-          <Text style={styles.titulo}>Cardapio</Text>
+            <View style={styles.ajustarTela}>
+              <Text style={styles.titulo}>Cardapio</Text>
 
-          <View style={styles.ajustarMenu}>
-            <Image
-              source={require("./src/imgs/expresso.jpg")}
-              style={styles.imagem} />
-            <Text style={styles.itemMenu}>Expresso</Text>
-          </View>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/expresso.jpg")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Expresso</Text>
+              </View>
 
-          <View style={styles.ajustarMenu}>
-            <Image
-              source={require("./src/imgs/latte.png")}
-              style={styles.imagem} />
-            <Text style={styles.itemMenu}>Latte</Text>
-          </View>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/latte.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Latte</Text>
+              </View>
 
-          <View style={styles.ajustarMenu}>
-            <Image
-              source={require("./src/imgs/macchiato.jpg")}
-              style={styles.imagem} />
-            <Text style={styles.itemMenu}>Macchiato</Text>
-          </View>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/macchiato.jpg")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Macchiato</Text>
+              </View>
 
-          <View style={styles.ajustarMenu}>
-            <Image
-              source={require("./src/imgs/mocca.png")}
-              style={styles.imagem} />
-            <Text style={styles.itemMenu}>Mocca</Text>
-          </View>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/mocca.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Mocaccino</Text>
+              </View>
 
-          <View style={styles.ajustarMenu}>
-            <Image
-              source={require("./src/imgs/interrogação.png")}
-              style={styles.imagem} />
-            <Text style={styles.itemMenu}>Especial</Text>
-          </View>
-        </View>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/frappe.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Frappe</Text>
+              </View>
 
-        <View style={styles.separador}></View>
-        <Button title='Contatos' onPress={() => navigation.navigate("Contato")} />
-        <Button title='Voltar' onPress={() => navigation.navigate("Home")} />
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/irish.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Irlandês</Text>
+              </View>
 
-      </ImageBackground>
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/martini.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Martini</Text>
+              </View>
+
+              <View style={styles.ajustarMenu}>
+                <Image
+                  source={require("./src/imgs/gelado.png")}
+                  style={styles.imagem} />
+                <Text style={styles.itemMenu}>Gelado</Text>
+              </View>
+            </View>
+
+            <View style={styles.separador}></View>
+            <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Contato")}>
+              <Text style={styles.botaoTexto}>Contato</Text>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Home")}>
+              <Text style={styles.botaoTexto}>Voltar</Text>
+            </TouchableOpacity>
+
+          </ImageBackground>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 
   function QuemSomos({ navigation }) {
     return (
+
       <ImageBackground
         source={require("./src/imgs/coffeeShop.jpg")}
         style={styles.backgroundImage}>
@@ -98,18 +137,22 @@ export default function App() {
         </View>
 
         <View style={styles.separador}></View>
-        <Button title='Voltar' onPress={() => navigation.navigate("Home")} />
+        <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.botaoTexto}>Voltar</Text>
+        </TouchableOpacity>
 
       </ImageBackground>
+
     )
   }
 
-  function Contato({navigation}) {
-    return(
+  function Contato({ navigation }) {
+    return (
+
       <ImageBackground
         source={require("./src/imgs/coffeeShop.jpg")}
         style={styles.backgroundImage}>
-<View style={styles.ajustarTela}>
+        <View style={styles.ajustarTela}>
           <Text style={styles.titulo}>Contatos</Text>
 
           <View style={styles.ajustarContato}>
@@ -125,14 +168,20 @@ export default function App() {
               style={styles.imagem} />
             <Text style={styles.itemContato}>(14)5555-1789</Text>
           </View>
-          
+
         </View>
 
         <View style={styles.separador}></View>
-        <Button title='Cardapio' onPress={() => navigation.navigate("Cardapio")} />
-        <Button title='Voltar' onPress={() => navigation.navigate("Home")} />
+
+        <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Cardapio")}>
+          <Text style={styles.botaoTexto}>Cardápio</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={0.8} style={styles.botaoForma} onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.botaoTexto}>Voltar</Text>
+        </TouchableOpacity>
 
       </ImageBackground>
+
     )
   }
 
@@ -143,7 +192,7 @@ export default function App() {
         <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
         <Stack.Screen name='Cardapio' component={Cardapio} options={{ headerShown: false }} />
         <Stack.Screen name='QuemSomos' component={QuemSomos} options={{ headerShown: false }} />
-        <Stack.Screen name='Contato' component={Contato} options={{headerShown: false}} />
+        <Stack.Screen name='Contato' component={Contato} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -157,6 +206,14 @@ const styles = StyleSheet.create({
 
   separador: {
     marginTop: 25,
+  },
+
+  separadorGrande: {
+    marginTop: 100,
+  },
+
+  separadorPequeno: {
+    marginTop: 10
   },
 
   ajustarTela: {
@@ -178,14 +235,33 @@ const styles = StyleSheet.create({
     fontSize: 28,
     textAlign: "center"
   },
-  
+
   imagem: {
     width: 100,
     height: 100,
     borderRadius: 100,
     marginBottom: 15
   },
-  
+
+  botaoForma: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 2,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    elevation: 3,
+    backgroundColor: '#1E90FF',
+  },
+
+  botaoTexto: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+
   //Cardapio
   ajustarMenu: {
     flexDirection: "row",
