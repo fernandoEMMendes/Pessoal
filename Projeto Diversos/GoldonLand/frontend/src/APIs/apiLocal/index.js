ef35e439d54eda5e436c751f9f0c4598605c7239
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-01-29 15:42:15 -0300

    29/01/2024

--- Projeto Diversos/GoldonLand/frontend/src/APIs/apiLocal/index.js
+++ Projeto Diversos/GoldonLand/frontend/src/APIs/apiLocal/index.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 
 const apiLocal = axios.create({
-    baseURL: "http://localhost:4444"
+    baseURL: "http://localhost:4444",
+    headers: {
+        Authorization: "Bearer " + `${token}`
+    }
 })
 
 export default apiLocal
+
+//Enviando o header no env não é necessário chama-lo em outras páginas
