cb9b764ed23b15eb0e63cdc7d00a56e47d1a02b0
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-01-30 14:26:13 -0300

    30/01/2024

--- Projeto Diversos/GoldonLand/frontend/src/pages/Inv/ListarInv/index.js
+++ Projeto Diversos/GoldonLand/frontend/src/pages/Inv/ListarInv/index.js
@@ -1,15 +1,66 @@
 import Header from "../../../components/Header"
+import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import apiLocal from "../../../APIs/apiLocal"
+import "./ListarInv.scss"
 
 export default function ListarInv(){
+    const navigation = useNavigate()
 
-    return(
+    const [listarInventario, setListarInventario] = useState([""])
+
+    const lsToken = localStorage.getItem("@GLToken2023")
+    const token = JSON.parse(lsToken)
+
+    useEffect(()=>{
+        async function verInventario(){
+            const resposta = await apiLocal.get("/ListarItem")
+            setListarInventario(resposta.data)
+        }
+        verInventario()
+    },[listarInventario])
+
+    return (
         <div>
             <div>
                 <Header />
             </div>
 
-            <div>
-                <h1>Listar Inv</h1>
+            <div className="alinharTabela">
+                <h1>Listar Inventário</h1>
+
+                <table className="caixa">
+                    <tr>
+                        <th>ID</th>
+                        <th>🎭NOME🎭</th>
+                        <th>🎫DESCRIÇÃO🎫</th>
+                        <th>🦾FORÇA🦾</th>
+                        <th>🧿PRECISAO🧿</th>
+                        <th>🛡DEFESA🛡</th>
+                        <th>👁‍🗨PC DE CRÍTICO👁‍🗨</th>
+                        <th>💫MP DE DANO💫</th>
+                        <th>🧧QUANTIDADE🧧</th>
+                        <th>ALTERAR</th>
+                        <th>DELETAR</th>
+                    </tr>
+                    {listarCategoria.map((resultados) => {
+                        return (
+                            <tr key={resultados.id}>
+                                <th>{resultados.id}</th>
+                                <th>{resultados.nome}</th>
+                                <th>{resultados.descricao}</th>
+                                <th>{resultados.forca}</th>
+                                <th>{resultados.precisao}</th>
+                                <th>{resultados.defesa}</th>
+                                <th>{resultados.pc_critico}</th>
+                                <th>{resultados.mp_dano}</th>
+                                <th>{resultados.quantidade}</th>
+                                <th>🖊</th>
+                                <th>❌</th>
+                            </tr>
+                        )
+                    })}
+                </table>
             </div>
         </div>
     )
