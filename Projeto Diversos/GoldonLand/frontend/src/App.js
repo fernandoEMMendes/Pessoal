ac22649588043e1f3124ab0a8dcefd0414a05a2a
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-03-21 17:02:09 -0300

    21/03/2024

--- Projeto Diversos/GoldonLand/frontend/src/App.js
+++ Projeto Diversos/GoldonLand/frontend/src/App.js
@@ -1,21 +1,25 @@
-import Rotas from "./routes"
-import "./App.scss"
-import AuthProvider from "./Contexts"
-import { ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css'
-import 'react-toastify/dist/ReactToastify.min.css'
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <AuthProvider>
-      <div className="align">
-        <ToastContainer 
-        autoClose={5000} 
-        position="top-right"
-        theme="dark"
-        />
-        <Rotas />
-      </div>
-    </AuthProvider>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
