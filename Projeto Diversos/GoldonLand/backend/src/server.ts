512c59269efcebb01dc4511c6dd68a7fda5b19ce
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-03-22 16:31:31 -0300

    22/03/2024

---
+++ Projeto Diversos/GoldonLand/backend/src/server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response, NextFunction } from "express"
+import "express-async-errors"
+import cors from "cors"
+import { rotas } from "./routes"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(rotas)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "Erro interno do servidor"
+    })
+
+})
+
+app.listen(7420, () => { console.log("Servidor iniciado na porta 7420 || =￣ω￣=") })
